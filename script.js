e6eb2da80431ea99102b5fa663aa9822a33fdee9
crysty72 <100445372+crysty72@users.noreply.github.com>
2024-06-11 16:02:07 -0300

    cambios

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 
-function agregar(valor){
-    document.getElementById('pantalla').value += valor
-}
-
-function borrar(){
-    document.getElementById('pantalla').value=''
-}
-function calcular(){
-
-    const valorpantalla = document.getElementById('pantalla').value
-    const resultado =eval(valorpantalla)
-    document.getElementById('pantalla').value= resultado
-}
+function agregar(valor) {
+    document.getElementById("pantalla").value += valor;
+  }
+  
+  function calcular() {
+    try {
+      let resultado = eval(document.getElementById("pantalla").value);
+      document.getElementById("pantalla").value = resultado;
+    } catch (e) {
+      document.getElementById("pantalla").value = "Error";
+    }
+  }
+  
+  function borrar() {
+    document.getElementById("pantalla").value = "";
+  }
+  
